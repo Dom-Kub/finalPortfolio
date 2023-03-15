d9d648c18049aa21e28af2ade3c409ab0e33e32f
Dom-Kub <dominik.kubala21@gmail.com>
2023-03-15 12:44:38 +0100

    Create project

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ParallaxContainer from './components/ParallaxContainer';
+
+function App() {
+  return (
+    <ParallaxContainer/>
+  );
+}
+
+export default App;
